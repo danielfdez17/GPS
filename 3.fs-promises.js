b0099a9f05a0082e3f45b2643b09a29c82020e37
danielfdez17 <daniel17dev@gmail.com>
2024-03-06 08:45:40 +0100

    promises

---
+++ 3.fs-promises.js
@@ -0,0 +1,20 @@
+// const { promisify } = require('node:util')
+// En los modulos nativos que no tienen promesas nativas
+// // * this can be used when it does not exist a promise version as if it would exist
+// const readFilePromises = promisify(fs.readFile) 
+// const fs = require('node:fs/promises')
+
+console.log('Leyendo el primer archivo ...')
+fs.readFile('./archivo.txt', 'utf-8')
+    .then(text => {
+        console.log('Primer texto: ', text)
+    })
+
+
+console.log('Hacer cosas mientras se lee el primer archivo ...')
+
+console.log('Leyendo el segundo archivo ...')
+fs.readFile('./archivo2.txt', 'utf-8')
+    .then(text => {
+        console.log('Segundo texto: ', text)
+    })
