f026cdf632f102bcff9a92ea21d4833c3a90500e
danielfdez17 <daniel17dev@gmail.com>
2024-03-06 08:36:47 +0100

    reading files in an asynchronous way

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,9 +1,15 @@
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo ...')
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
-console.log(text)
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
+    console.log(text)
+
+})
+
+console.log('Hacer cosas mientras se lee el primer archivo ...')
 
 console.log('Leyendo el segundo archivo ...')
-const text2 = fs.readFileSync('./archivo2.txt', 'utf-8')
-console.log(text2)
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+    console.log(text)
+
+})
