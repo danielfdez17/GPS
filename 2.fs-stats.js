53d7cacfd2556e35aee94b361af351ed69d874de
danielfdez17 <daniel17dev@gmail.com>
2024-03-06 08:24:14 +0100

    file stats

---
+++ 2.fs-stats.js
@@ -0,0 +1,9 @@
+const fs = require('node:fs') // ! recomendado a partir de Node 16 poner 'node:"module"'
+const stats = fs.statSync('./archivo.txt')
+
+console.log(
+    stats.isFile(),
+    stats.isDirectory(),
+    stats.isSymbolicLink(),
+    stats.size,
+);
