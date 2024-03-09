80afe96d6e7184a04204da6b51623139d82576c2
danielfdez17 <daniel17dev@gmail.com>
2024-03-09 18:50:59 +0100

    fs async await module js

--- 4.fs-async-await.js
+++ 4.fs-async-await.js
@@ -4,23 +4,20 @@
 // const readFilePromises = promisify(fs.readFile) 
 
 
-const { readFile } =  require('node:fs/promises')
+const { readFile } =  require('node:fs/promises');
 
 // * funcion autoinvocada
 // ! IIFE - Immediately Invoked Function Expression
 (
-    async () => {}
-
+    async () => {
+        console.log('Leyendo el primer archivo ...')
+        const text = await readFile('./archivo.txt', 'utf-8')
+        console.log('Primer texto: ', text)
+        
+        console.log('Hacer cosas mientras se lee el primer archivo ...')
+        
+        console.log('Leyendo el segundo archivo ...')
+        const text2 = await readFile('./archivo2.txt', 'utf-8')
+        console.log('Segundo texto: ', text2)
+    }
 )()
-
-console.log('Leyendo el primer archivo ...')
-const text = await readFile('./archivo.txt', 'utf-8')
-console.log('Primer texto: ', text)
-
-
-
-console.log('Hacer cosas mientras se lee el primer archivo ...')
-
-console.log('Leyendo el segundo archivo ...')
-const text2 = await readFile('./archivo2.txt', 'utf-8')
-console.log('Segundo texto: ', text2)
