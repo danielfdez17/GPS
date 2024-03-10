5c5c855a27b35c85eb5d1c08cb3acbbe4c3c4425
danielfdez17 <daniel17dev@gmail.com>
2024-03-10 20:01:16 +0100

    added eslint extension for JS

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -9,46 +9,44 @@
 //     files.forEach(file => {
 //         console.log(file);
 //     })
-    
 // });
 
-const fs = require("node:fs/promises");
-const path = require("node:path");
+const fs = require('node:fs/promises')
+const path = require('node:path')
+const pc = require('picocolors')
 
 const folder = process.argv[2] ?? '.'
 
 async function ls (folder) {
-    let files;
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(pc.red(`No se pudo leer el directorio ${folder}`))
+    process.exit(1)
+  }
+
+  const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
     try {
-        files = await fs.readdir(folder)
+      stats = await fs.stat(filePath) // ? info del archivo
     } catch {
-        console.error(`No se pudo leer el directorio ${folder}`);
-        process.exit(1);
+      console.error(`No se pudo leer el archivo ${filePath}`)
+      process.exit(1)
     }
 
-    const filesPromises = files.map(async file => {
-        const filePath = path.join(folder, file);
-        let stats;
-        try {
-            stats = await fs.stat(filePath); // ? info del archivo
-        } catch {
-            console.error(`No se pudo leer el archivo ${filePath}`);
-            process.exit(1);
-        }
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size
+    const fileModified = stats.mtime.toLocaleString()
 
-        const isDirectory = stats.isDirectory();
-        const fileType = isDirectory ? 'd' : 'f'
-        const fileSize = stats.size;
-        const fileModified = stats.mtime.toLocaleString();
+    return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.toString().padStart(10))} ${pc.yellow(fileModified)}`
+  })
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`;
-    });
+  const filesInfo = await Promise.all(filesPromises)
 
-    const filesInfo = await Promise.all(filesPromises);
-
-    filesInfo.forEach(fileInfo => console.log(fileInfo));
-
+  filesInfo.forEach(fileInfo => console.log(fileInfo))
 }
 
-ls(folder);
-
+ls(folder)
