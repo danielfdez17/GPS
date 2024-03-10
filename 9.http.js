70693742fc6b5af8f81cb8590a8f69f34ea9a808
danielfdez17 <daniel17dev@gmail.com>
2024-03-10 20:30:31 +0100

    using created functionalities

--- 9.http.js
+++ 9.http.js
@@ -1,10 +1,15 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10.free-port.js')
+
+const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hello world')
 })
 
-server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`)
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${server.address().port}`)
+  })
 })
