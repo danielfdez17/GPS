3db2f215be43b6914f1cd6da6bb4754e2a9daefa
danielfdez17 <daniel17dev@gmail.com>
2024-03-12 15:25:24 +0100

    how api works

--- clase-2/2.routing.js
+++ clase-2/2.routing.js
@@ -13,13 +13,32 @@ const processRequest = (req, res) => {
           return res.end(JSON.stringify(dittoJSON))
         default:
           res.statusCode = 404
-          res.setHeader('Content-Type', 'text/html; charset = utf - 8')
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
           return res.end('<h1>404</h1>')
       }
     case 'POST':
       switch (url) {
-        case '/pokemon':
-          const body = ''
+        case '/pokemon': {
+          let body = ''
+
+          // 1. escuchar el evento data
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          return res.end('404 Not Found')
       }
   }
 }
