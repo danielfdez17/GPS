8f931295b9a97addce60c5cf3a821339fe2cce84
danielfdez17 <daniel17dev@gmail.com>
2024-03-16 20:45:33 +0100

    get and post methods implemented

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,14 +1,61 @@
 const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powerede-by')
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'hola mundo' })
+// Todos los recursos que sean MOVIES se identifican con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLocaleLowerCase() === genre.toLocaleLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    title,
+    genre,
+    director,
+    year,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+
+  // Esto no seria REST porque se guarda el estado de la app en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie) // para actualizar la cache del cliente
 })
 
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
-  console.log(`server listening on port https://localhost:${PORT}`)
+  console.log(`server listening on port http://localhost:${PORT}`)
 })
