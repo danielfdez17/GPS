636a538d9fa9e255b9d61356621502784f46547c
danielfdez17 <daniel17dev@gmail.com>
2024-03-17 10:35:43 +0100

    lesson 3 finished

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,14 +1,45 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
+
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'http://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+  }
+}))
 app.disable('x-powerede-by')
+
+// metodos normales: GET/HEAD/POST
+// metodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight requiere una peticion especial llamada OPTIONS
 
 // Todos los recursos que sean MOVIES se identifican con /movies
 app.get('/movies', (req, res) => {
+  // const origin = req.header('origin')
+  // // cuando la peticion es del mismo ORIGIN, no se envia la cabedera ORIGIN
+  // // http://localhost:1234 --> http://localhost:1234
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -17,6 +48,26 @@ app.get('/movies', (req, res) => {
     return res.json(filteredMovies)
   }
   res.json(movies)
+})
+
+app.delete('/movies/:id', (res, req) => {
+  // const origin = req.header('origin')
+  // // cuando la peticion es del mismo ORIGIN, no se envia la cabedera ORIGIN
+  // // http://localhost:1234 --> http://localhost:1234
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 app.get('/movies/:id', (req, res) => { // path-to-regexp
@@ -68,6 +119,17 @@ app.patch('/movies/:id', (req, res) => {
 
   return res.json(updateMovie)
 })
+
+// app.options('/movies:id', (req, res) => {
+//   const origin = req.header('origin')
+//   // cuando la peticion es del mismo ORIGIN, no se envia la cabedera ORIGIN
+//   // http://localhost:1234 --> http://localhost:1234
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+//   res.send(200)
+// })
 
 const PORT = process.env.PORT ?? 1234
 
